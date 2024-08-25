4497fd498581deaeab16a10ce7d6b9303a25496f
RahulSM2002 <156082731+RahulSM2002@users.noreply.github.com>
2024-08-25 14:31:19 +0530

    Login API integrated

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const app = express();
-const mongooose = require("mongoose");
+const mongoose = require("mongoose"); // Fixed typo here
 const bcrypt = require("bcryptjs");
-
+const jwt = require("jsonwebtoken");
 app.use(express.json());
 
 const mongoUrl =
   "mongodb+srv://rahulraddi4:8095341540sm@cluster0.v3tto.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-mongooose
+
+mongoose // Corrected here as well
   .connect(mongoUrl)
   .then(() => {
     console.log("Connected to DB");
@@ -16,14 +17,17 @@ mongooose
     console.log(err);
   });
 
+const JWT_SECRET =
+  "kejfncvwqlkemxndkb r40ru094jrnfoi1ubf90j4-r9h039irei4hr080924ru09@*(U#@UH#DIUHiuheujghvwqlugkuwf86iysvtf86saf7tavaao7fa7vco76f7stav76fs7vsa7tc7ta7isctibgv28h9ug38w2ij9ehoiwh9eh0h93uw7g79h^%R*T&%DCUGU";
 require("./userDetails");
 
-const User = mongooose.model("UserInfo");
+const User = mongoose.model("UserInfo");
 
 app.get("/", (req, res) => {
   res.send({ status: "Started" });
 });
 
+// Sign-Up API
 app.post("/register", async (req, res) => {
   const { name, email, mobile, password } = req.body;
 
@@ -38,12 +42,30 @@ app.post("/register", async (req, res) => {
     await User.create({
       name: name,
       email: email,
-      mobile,
-      encryptedPassword,
+      mobile: mobile,
+      password: encryptedPassword, // Note the field name used here
     });
     res.send({ status: "ok", data: "User Created Successfully" });
   } catch (error) {
     res.send({ status: "error", data: error });
+  }
+});
+
+// Sign-In API
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  const oldUser = await User.findOne({ email: email });
+
+  if (!oldUser) {
+    return res.send({ data: "User Not Found, Please check your email id" });
+  }
+
+  // Ensure you are accessing the correct field
+  if (await bcrypt.compare(password, oldUser.password)) {
+    const token = jwt.sign({ email: oldUser.email }, JWT_SECRET);
+    return res.status(201).send({ status: "ok", token: token });
+  } else {
+    return res.status(401).send({ status: "error", data: "Invalid password" });
   }
 });
 
